3bfae605a9116af4d4738c3e2dcb7c8d78962b5f
golfoclock <cmmc@protonmail.ch>
2018-04-24 23:10:29 -0400

    add if statement

--- encrypt.py
+++ encrypt.py
@@ -6,4 +6,7 @@ while choice != 0:
 	choice = input("\nWould you like to encrypt or decrypt the message?\nEnter 1 to encrypt, or 2 to decrypt. Enter 0 to exit program.")
 
 	if choice == '1':
-		message = input("Enter message for encryption")
+		message = input("Enter message for encryption:")
+		for i in range(0, len(message)):
+			result = result + chr(ord(message[i]) - 2)
+			
