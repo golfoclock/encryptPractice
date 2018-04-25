85b747a7e827a0fb78fc656da37f9029351346c2
golfoclock <cmmc@protonmail.ch>
2018-04-24 23:00:14 -0400

    begin while loop

--- encrypt.py
+++ encrypt.py
@@ -5,5 +5,5 @@ choice = ''
 while choice != 0:
 	choice = input("\nWould you like to encrypt or decrypt the message?\nEnter 1 to encrypt, or 2 to decrypt. Enter 0 to exit program.")
 
-	if choice == '1'
+	if choice == '1':
 		message = input("Enter message for encryption")
