7804d54cd5f3bf40e6d4e9aa8050f73231024ae0
golfoclock <cmmc@protonmail.ch>
2018-04-24 23:20:17 -0400

    add message for decryption

--- encrypt.py
+++ encrypt.py
@@ -6,7 +6,14 @@ while choice != 0:
 	choice = input("\nWould you like to encrypt or decrypt the message?\nEnter 1 to encrypt, or 2 to decrypt. Enter 0 to exit program.")
 
 	if choice == '1':
-		message = input("Enter message for encryption:")
+		message = input("\nEnter message for encryption: ")
 		for i in range(0, len(message)):
 			result = result + chr(ord(message[i]) - 2)
-			
+
+		print(result + '\n\n')
+		result = ''
+
+	elif choice == '2':
+		message = input("\nEnter a mesage for decryption: ")
+		for i in range(0, len(message)):
+		result = result + chr(ord(message[i] + 2))	
